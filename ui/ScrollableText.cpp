1eda0889ed3642da8aba2b9cdce49843b6ccff05
Denis Colesnicov <eugustus@gmail.com>
2018-01-13 21:17:44 +0100

    Optimizace. Odstranena chyba s orezavanim retezce, misto toho je ted chyba s 'buffer overflow'!

--- ui/ScrollableText.cpp
+++ ui/ScrollableText.cpp
@@ -14,15 +14,17 @@ namespace ui
                 TextualElement(),
                 //m_padding(5),
                 m_rowsCount(0),
-                m_start_row(0),
+                //m_rowsLength(),
+                //m_start_row(0),
                 m_visibled_rows(0),
                 m_lineHeight(0)
     {
+        m_rowsLength = new int();
     }
 
     ScrollableText::~ScrollableText()
     {
-        // TODO Auto-generated destructor stub
+        delete m_rowsLength;
     }
 
     bool ScrollableText::Draw(Adafruit_GFX* tft, bool current, Action action,
@@ -31,44 +33,35 @@ namespace ui
             uint8_t tpos_div)
     {
 
-        int8_t padding = 5;
         uint16_t text_width, text_height;
+
         {
             int16_t tmp;
             tft->getTextBounds((char*) m_text, m_pos_x, m_pos_y, &tmp, &tmp,
                     &text_width, &text_height);
         }
-        uint32_t text_length = strlen(m_text);
-        uint16_t char_per_row = ((m_width - 12 - (padding * 3))
-                / (text_width / text_length)) - 2;
 
-        int rows = 1;
-        if (text_length > char_per_row)
-        {
-            rows = text_length / char_per_row;
-        }
+        recalculate(text_width);
 
-        static uint16_t start_row = 0;
         if (current && action == Action::ENTER)
         {
             setInvalid();
             setActive(!isActive());
         }
         else if (isActive() && action == Action::DOWN
-                && (rows - start_row) >= start_row)
+                && (m_rowsCount - m_start_row) >= m_start_row)
         {
-            start_row++;
+            m_start_row++;
             setInvalid();
         }
-        else if (isActive() && action == Action::UP && start_row > 0)
+        else if (isActive() && action == Action::UP && m_start_row > 0)
         {
-            start_row--;
+            m_start_row--;
             setInvalid();
         }
         else if ((getLastState() || current)
                 && (action == Action::UP || action == Action::DOWN))
         {
-            Serial.println("reload");
             setInvalid();
         }
 
@@ -81,98 +74,114 @@ namespace ui
 
             uint16_t scrollHeight, scroll_row = 0;
 
-            if (m_height - (padding * 2) > (text_height * rows))
+            if (m_height - (PADDING * 2) > (text_height * m_rowsCount))
             {
                 scrollHeight = m_height;
             }
             else
             {
-                scrollHeight = ((float) (m_height-16) / ((text_height + 2) * rows)) * 100;
-                scroll_row = ((float) text_height / (m_height+10)) * 100;
+                scrollHeight = ((float) (m_height - 16)
+                        / ((text_height + 2) * m_rowsCount)) * 100;
+                scroll_row = ((float) text_height / (m_height + 10)) * 100;
 
             }
 
             tft->fillRect(m_pos_x + m_width - 8,
-                    m_pos_y + (start_row * scroll_row) + 1, 6, scrollHeight,
+                    m_pos_y + (m_start_row * scroll_row) + 1, 6, scrollHeight,
                     (isActive()) ? colorFr_a : colorFr);
 
             tft->setTextWrap(false);
 
             text_height += 2;
-            uint16_t visibled_rows = ((m_height - (padding * 2)) / text_height); //-1;
-
-            int zustatek = text_length - (rows * char_per_row);
-
-            unsigned int index = 0, tmp_index;
-            if (zustatek > 0)
-            {
-                rows++;
-                m_rowsLength[m_rowsCount] = zustatek;
-            }
-            char text_[rows][char_per_row + 1];
-            uint16_t keys[rows];
-
-            for (int i = 0; rows > i; i++)
-            {
-                tmp_index =
-                        (i == rows) ? index + zustatek : index + char_per_row;
-                while ((rows > i) && (m_text[tmp_index] != ' ' || tmp_index > 0))
-                {
-
-                    if (m_text[tmp_index] == ' ')
-                    {
-                        break;
-                    }
-                    tmp_index--;
-                }
-
-                int ii = 0;
-
-                for (; index < tmp_index; index++)
-                {
-                    if (ii == 0 && m_text[index] == ' ')
-                    {
-                        continue;
-                    }
-                    text_[i][ii] = m_text[index];
-                    ii++;
-                }
-                keys[i] = ii;
-            }
+            uint16_t visibled_rows = ((m_height - (PADDING * 2)) / text_height); //-1;
 
             uint16_t label_x;
             uint16_t label_y;
 
             label_y = m_pos_y + text_height;
-            label_x = m_pos_x + padding;
+            label_x = m_pos_x + PADDING;
 
-            tft->setTextColor(colorFg);
+            tft->setTextColor((isActive()) ? colorFg_a : colorFg);
 
-            uint16_t i = start_row;
+            uint16_t i = m_start_row;
             uint16_t steps;
-            if (rows - start_row > visibled_rows)
+            if (m_rowsCount - m_start_row > visibled_rows)
             {
                 steps = visibled_rows;
             }
             else
             {
-                steps = (rows - start_row); // - visibled_rows;
+                steps = (m_rowsCount - m_start_row); // - visibled_rows;
+            }
+            uint16_t offset = 0;
+
+            if (m_start_row > 0)
+            {
+                for (int i = 0; i < m_start_row; i++)
+                {
+                    offset += m_rowsLength[i];
+                }
             }
 
             for (int factor = 0; factor < steps; i++, factor++)
             {
-                Serial.print("i: ");
-                Serial.println(i);
                 tft->setCursor(label_x, label_y + (text_height * factor));
-                for (int k = 0; k < keys[i]; k++)
-                {
-                    tft->print(text_[i][k]);
-                }
+
+                char row[m_rowsLength[i] + 1];
+                strlcpy(row, m_text + offset, m_rowsLength[i] + 1);
+                offset += m_rowsLength[i];
+                tft->print(row);
             }
             setValid();
         }
-        setLastState(current);
+        setLastState(isActive());
         return isActive();
+    }
+
+    void ScrollableText::setText(char* text)
+    {
+        {
+            TextualElement::setText(text);
+            m_recalculate = true;
+        }
+    }
+
+    void ScrollableText::recalculate(uint16_t char_width)
+    {
+        if (m_recalculate)
+        {
+
+            uint32_t text_length;
+            uint16_t char_per_row;
+            text_length = strlen(m_text);
+            char_per_row = ((m_width - 12 - PADDING)
+                    / (char_width / text_length));
+            uint32_t globalI = 0;
+            while (text_length > globalI)
+            {
+                uint16_t i = globalI + char_per_row;
+                if (i > text_length)
+                {
+                    i -= (i - text_length);
+                }
+
+                int ii = char_per_row + 1;
+                int iii = 0;
+                while (m_text[i - iii] != ' ' || iii <= 17)
+                {
+                    if (m_text[i - iii] == ' ' || i == text_length)
+                    {
+                        m_rowsLength[m_rowsCount] = ii;
+                        break;
+                    }
+                    ii--;
+                    iii++;
+                }
+                m_rowsCount++;
+                globalI += ii;
+            }
+            m_recalculate = false;
+        }
     }
 
 } /* namespace ui */
--- ui/ScrollableText.h
+++ ui/ScrollableText.h
@@ -1,21 +1,24 @@
 /*
- * ScrollableText.h
+ * File: ScrollableText.h
+ * Created on: 27. 12. 2017
+ * Author: Denis Colesnicov <eugustus@gmail.com>
+ * Version: 1.2.2
  *
- *  Created on: 27. 12. 2017
- *      Author: denis
+ * @note BETA verion, less tested!
  */
 
 #pragma once
 
 #include <TextualElement.hpp>
 
-#include "screen_config.h"
+#define PADDING 5
 
 namespace ui
 {
 
     /*
-     *
+     * Prvek ktery umoznuje skrolovat text v nem.
+     * @note BETA
      */
     class ScrollableText: public TextualElement
     {
@@ -23,55 +26,57 @@ namespace ui
         ScrollableText();
 
         ~ScrollableText();
-
-
-        bool Draw(Adafruit_GFX* tft, bool current, Action action =
-                Action::NONE);
-
+        /**
+         * Vykresli prvek
+         *
+         * @param tft Ukazatel na instanci displeje
+         * @param current Indikator, zda prvek ma zamereni (focus)
+         * @param action Akce uzivatele
+         * @param colorBg Barva pozadi prvku
+         * @param colorFr Barva ohraniceni prvku
+         * @param colorFg Barva textu prvku
+         * @param colorBg_a Barva pozadi prvku kdyz prvek ma zamereni (focus)
+         * @param colorFr_a Barva ohraniceni prvku kdyz prvek ma zamereni (focus)
+         * @param colorFg_a Barva textu prvku kdyz prvek ma zamereni (focus)
+         * @param tpos_div Delic pro vertikalni pozicovani textu. Cim vetsi cislo, tim vic nahore.
+         *
+         * @return bool TRUE pokud je prvek aktivni nebo zpracoval udalost, jinak False
+         *
+         * @todo Buffer overflow - Pri vypisu textu, saha mimo svuj pmetovy zasobnik a
+         *       vypisuje hodnoty pameti z 'ciziho' sektoru
+         * @todo Pri prvnim aktivovani prvku se text rozhodi!
+         */
         bool Draw(Adafruit_GFX *tft, bool current, Action action,
                 uint16_t colorBg, uint16_t colorFr, uint16_t colorFg,
                 uint16_t colorBg_a, uint16_t colorFr_a, uint16_t colorFg_a,
                 uint8_t tpos_div = 0);
 
+        /**
+         * Nastavi text
+         *
+         * @param text Text pro prvek
+         */
+        void setText(char* text);
 
-        void drawBox(/*uint16_t textHeight*/);
+        /**
+         * Prepocita pocet radku a optimalni pocet znaku na radek
+         *
+         * @param char_width Sirka znaku
+         */
+        void recalculate(uint16_t char_width);
 
     private:
 
         int m_rowsCount;
-        int m_rowsLength[];
-        uint16_t m_start_row;
+        int *m_rowsLength;
 
         bool m_recalculate = true;
         uint16_t m_visibled_rows;
 
         uint8_t m_lineHeight;
+        uint16_t m_start_row = 0;
 
     };
 
 } /* namespace ui */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
