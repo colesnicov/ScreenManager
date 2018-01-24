b0a69c53cf76c8e68f087454a51d09266dcef3d0
Denis Colesnicov <eugustus@gmail.com>
2018-01-24 17:11:02 +0100

    Pridan hlavickovy soubor pro vkladani vsech dulezitych hlavicek

---
+++ ScreenManager.h
@@ -0,0 +1,19 @@
+/*
+ * Include all TOP LEVEL header files
+ *
+ * File: ScreenManager.h
+ *
+ * Created on: 24. 1. 2018
+ * Author: Denis Colesnicov <eugustus@gmail.com>
+ * Version 1.2.2
+ */
+
+#pragma once
+
+#include <Screen.h>
+#include <Colors.h>
+#include <Handler.hpp>
+#include <ui/Button.hpp>
+#include <ui/ScrollableText.h>
+#include <ui/Spinner.h>
+#include <ui/ToggleButton.h>
