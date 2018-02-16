1286917d50f0e5a44f7bcba7f9ddeda8c24e59ea
Denis Colesnicov <eugustus@gmail.com>
2018-02-16 19:40:22 +0100

    Pridan prvek RangeInt

---
+++ ui/RangeInt.cpp
@@ -0,0 +1,135 @@
+#include <ui/RangeInt.h>
+
+namespace ui
+{
+
+    RangeInt::RangeInt() :
+                m_value(0),
+                m_changed(false)
+    {
+
+    }
+
+    RangeInt::~RangeInt()
+    {
+    }
+
+    bool RangeInt::Draw(Adafruit_GFX* tft, bool current, Action action,
+            uint16_t colorBg, uint16_t colorFr, uint16_t colorFg,
+            uint16_t colorBg_a, uint16_t colorFr_a, uint16_t colorFg_a,
+            uint8_t tpos_div)
+    {
+
+        if (current && action == Action::ENTER)
+        {
+            setInvalid();
+            setActive(!isActive());
+        }
+        else if ((getLastState() != current)
+                && (action == Action::UP || action == Action::DOWN))
+        {
+            setInvalid();
+        }
+
+        if (current && isActive())
+        {
+            if (action == Action::RIGHT)
+            {
+                if (*m_value < m_max)
+                {
+                    *m_value += 1;
+                    m_changed = true;
+                    setInvalid();
+                }
+                else if (m_dokola)
+                {
+                    *m_value = m_min;
+                    m_changed = true;
+                    setInvalid();
+                }
+            }
+            else if (action == Action::LEFT)
+            {
+                if (*m_value > m_min)
+                {
+                    *m_value -= 1;
+                    m_changed = true;
+                    setInvalid();
+                }
+                else if (m_dokola)
+                {
+                    *m_value = m_max;
+                    m_changed = true;
+                    setInvalid();
+                }
+            }
+        }
+
+        if (isInvalid())
+        {
+
+            tft->fillRect(m_pos_x, m_pos_y, m_width, m_height,
+                    (current) ? colorBg_a : colorBg);
+            tft->drawRect(m_pos_x, m_pos_y, m_width, m_height,
+                    (current) ? colorFr_a : colorFr);
+            tft->drawFastVLine(m_pos_x + m_height, m_pos_y + 1, m_height - 2,
+                    (current) ? colorFr_a : colorFr);
+            tft->drawFastVLine(m_pos_x + m_width - m_height, m_pos_y + 1,
+                    m_height - 2, (current) ? colorFr_a : colorFr);
+
+            tft->fillRect(m_pos_x + 3, (m_pos_y + (m_height / 2) - 1),
+                    m_height - 6, 3, (isActive()) ? colorFr_a : colorFr);
+
+            // +
+            tft->fillRect(m_pos_x + m_width + 3 - m_height,
+                    (m_pos_y + (m_height / 2) - 1), m_height - 6, 3,
+                    (isActive()) ? colorFr_a : colorFr);
+            tft->fillRect(m_pos_x + m_width - (m_height / 2) - 2, m_pos_y + 3,
+                    3, m_height - 6, (isActive()) ? colorFr_a : colorFr);
+
+            uint8_t val_len = intLength(*m_value) + 1;
+            char val[val_len];
+            snprintf(val, strlen(m_text) + val_len, m_text, *m_value);
+
+            uint16_t text_width, text_height;
+
+            {
+                int16_t tmp;
+                tft->getTextBounds(val, m_pos_x, m_pos_y, &tmp, &tmp,
+                        &text_width, &text_height);
+            }
+
+            uint8_t box_height_half = m_height / 2;
+            uint8_t text_height_half = text_height / 2;
+            uint16_t label_y;
+            if (box_height_half > text_height_half)
+            {
+                if (tpos_div > 0)
+                {
+                    label_y = m_pos_y + box_height_half
+                            + text_height / tpos_div;
+                }
+                else
+                {
+                    label_y = m_pos_y + (box_height_half - text_height_half);
+                }
+            }
+            else
+            {
+                label_y = m_pos_y + m_height - text_height_half;
+            }
+
+            tft->setTextColor((isActive()) ? colorFg_a : colorFg);
+            tft->setCursor((m_pos_x + (m_width / 2)) - (text_width / 2),
+                    label_y);
+            tft->print(val);
+
+            setValid();
+        }
+
+        setLastState(current);
+        return isActive();
+    }
+
+} /* namespace ui */
+
---
+++ ui/RangeInt.h
@@ -0,0 +1,139 @@
+#pragma once
+
+/*
+ * File: Spinner.h
+ * Created on: 3. 1. 2018
+ * Author: Denis Colesnicov <eugustus@gmil.com>
+ * Version: 1.2.1
+ */
+
+#include <TextualElement.hpp>
+
+namespace ui
+{
+
+    /*
+     * Inkrementlni/Dekrementalni spinner pro cele cislo
+     *
+     * @todo Implementovat maximalni a minimalni limit pro hodnotu
+     */
+    class RangeInt: public TextualElement
+    {
+    public:
+        RangeInt();
+
+        virtual ~RangeInt();
+
+        /**
+         * Vykresli prvek
+         *
+         * @param *tft Ukazatel na instanci displeje
+         * @param current Priznak jestli je tento prvek "pod kurzorem"
+         * @param action Aktulni akce tlacitek
+         * @param colorBg Barva pozadi prvku
+         * @param colorFr Barva ohraniceni prvku
+         * @param colorFg Barva textu prvku
+         * @param colorBg_a Barva pozadi prvku kdyz prvek ma zamereni (focus)
+         * @param colorFr_a Barva ohraniceni prvku kdyz prvek ma zamereni (focus)
+         * @param colorFg_a Barva textu prvku kdyz prvek ma zamereni (focus)
+         * @param tpos_div Delic pro korekci vertikalni pozice textu, cim vetsi cislo tim vic nahore
+         *
+         * @return bool TRUE pokud je prvek aktivni nebo zpracoval udalost, jinak False
+         */
+        bool Draw(Adafruit_GFX *tft, bool current, Action action,
+                uint16_t colorBg, uint16_t colorFr, uint16_t colorFg,
+                uint16_t colorBg_a, uint16_t colorFr_a, uint16_t colorFg_a,
+                uint8_t tpos_div = 0);
+
+        /**
+         * Nastavi hodnotu referenci
+         *
+         * @param *value Ukazatel na hodnotu
+         */
+        void setValue(int *value)
+        {
+            m_value = value;
+        }
+
+        /**
+         * Spocte pocet znaku v cisle. Znamenko "-" je take do poctu!
+         *
+         * @param x Cislo
+         *
+         * @return uint8_t Delka cisla
+         */
+        short intLength(int x)
+        {
+            if (x >= 1000)
+            {
+                return 4;
+            }
+            else if (x >= 100)
+            {
+                return 3;
+            }
+            else if (x >= 10)
+            {
+                return 2;
+            }
+            else if (x >= 0)
+            {
+                return 1;
+            }
+            else if (x <= -1000)
+            {
+                return 5;
+            }
+            else if (x <= -100)
+            {
+                return 4;
+            }
+            else if (x <= -10)
+            {
+                return 3;
+            }
+            else if (x < 0)
+            {
+                return 2;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        /**
+         * Volat pouze jednou!!
+         */
+        bool isChanged()
+        {
+            bool c = m_changed;
+            m_changed = !m_changed;
+            return c;
+        }
+
+        void setMin(int min)
+        {
+            m_min = min;
+        }
+
+        void setMax(int max)
+        {
+            m_max = max;
+        }
+
+        void setDokola(bool dokola)
+        {
+            m_dokola = dokola;
+        }
+
+    private:
+        int* m_value;
+        bool m_changed;
+        bool m_dokola;
+        int m_min;
+        int m_max;
+    };
+
+} /* namespace ui */
+
